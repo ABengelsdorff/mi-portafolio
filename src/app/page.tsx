029122adb6bce42dd8c217cda9dbca573a463c57
ABengelsdorff <Angelica.bengelsdorff.5@gmail.com>
2025-04-19 17:53:25 -0300

    mis habilidades funcional

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import Projects from "./components/projects";
+import Skills from "./components/skills";
 
 export default function Home() {
   return <div className="relative z-10 min-h-screen bg-transparent">
+    <Skills />
     <Projects />
   </div>;
 }
