bafaf950dc1a221405417dd3b94f26ea3cd71fd1
ABengelsdorff <Angelica.bengelsdorff.5@gmail.com>
2025-04-19 19:35:34 -0300

    footer funcional

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Nav from "./components/navbar/nav";
 import Projects from "./components/projects";
 import Skills from "./components/skills";
+import Footer from "./components/footer";
 
 export default function Home() {
   return <div className="relative z-10 min-h-screen bg-transparent">
     <Skills />
     <Projects />
     <Nav />
+    <Footer />
   </div>;
 }
