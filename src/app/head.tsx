593ce6087f5e2baf867febafad6ac09c9a9f489b
ABengelsdorff <Angelica.bengelsdorff.5@gmail.com>
2025-04-23 18:52:15 -0300

    nuevo icono actualizado 3

---
+++ src/app/head.tsx
@@ -0,0 +1,19 @@
+export default function Head() {
+    return (
+      <>
+        <title>Angelica Bengelsdorff | Desarrolladora Full Stack</title>
+        <meta
+          name="description"
+          content="Soy Angelica Bengelsdorff, desarrolladora Full Stack enfocada en crear experiencias web modernas, atractivas y funcionales."
+        />
+        <meta
+          name="keywords"
+          content="Angelica Bengelsdorff, portafolio, desarrolladora web, full stack developer, React, Next.js, Frontend, Backend"
+        />
+        <meta name="author" content="Angelica Bengelsdorff" />
+        <meta name="creator" content="Angelica Bengelsdorff" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </>
+    );
+  }
+  
